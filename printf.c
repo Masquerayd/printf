23ddcbb8778f55a9d46a00f62b0885fb1ee0064b
Masquerayd <philanimanatha@outlook.com>
2023-11-13 22:48:18 +0000

    Managed to get the struct working

--- printf.c
+++ printf.c
@@ -1,39 +1,23 @@
 #include "functions.h"
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * _printf - printf the format string with the arguments 
- * @format: format string
- * Returns: The number of characters printed
- */
+
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	int count = 0;
-	char *(*func)(const char *, specvalue);
-	char *str;
-	specvalue svalue;
-
-	va_start(arg, format);
-	svalue.str = "hello";
-
-/*
- * for loop checks for the spercifier and then passes the specifier int the function
- * The function then returns a function that will process the argument by adding the 
- * procced argument to the buffer
- * for now we will just print the functions to see that we are enterting all of them
- */
-
-	for (count = 0; *(format + count) != '\0'; count++)
+	int count;
+	char *(*f)(const char *, specvalue);
+	specvalue speval; 
+	speval.str = "hello";
+	
+	for (count = 0 ; format[count] != '\0'; count++)
 	{
-		if (*(format + count) == '%')
+		if (format[count] == '%')
 		{
-			func = get_func(format[count + 1]);
-			str = func(format, svalue);
-			printf("%p",str);
+			f = get_func(format[count + 1]);
+			f(format, speval);
 		}
 	}
-	return (0);
-
+	return (count);
 }
 
+
+
+	
