492e63b579c35c4703bbd9b60cf4c0b21b1b068c
Masquerayd <philanimanatha@outlook.com>
2023-11-20 20:10:18 +0000

    checking percent sign

--- main.c
+++ main.c
@@ -11,12 +11,12 @@ int main(void)
 {
         int len, len2;
 
-        len = _printf(NULL);
-        len2 = printf(NULL);
+        len = _printf("%s", "hello");
+        len2 = 5;
         fflush(stdout);
         if (len != len2)
         {
-                printf("Lengths differ[%d][%d].\n", len, len2);
+                _printf("Lengths differ[%r][%k].\n");
                 fflush(stdout);
                 return (1);
         }
